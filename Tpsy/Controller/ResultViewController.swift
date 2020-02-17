24a3802e97d9e0b1ddcd23d7e2e4098be5f3d986
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-16 19:22:43 -0600

    Define IBActions/IBOutlets
    For the TipViewCalculator view controller, define IBAction and IBOutlet stubs.
    
    Related to: none

---
+++ Tpsy/Controller/ResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultViewController.swift
+//  Tpsy
+//
+//  Created by Darren Rambaud on 2/16/20.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tpsy/Controller/TipViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Tpsy
+//
+//  Created by Darren Ramabud on 02/16/2020.
+//  Copyright © 2020 Darren Rambaud. All rights reserved.
+//
+
+import UIKit
+
+class TipViewController: UIViewController {
+
+    @IBOutlet weak var total: UITextField!
+    
+    @IBOutlet weak var zeroPercent: UIButton!
+    
+    @IBOutlet weak var tenPercent: UIButton!
+    
+    @IBOutlet weak var twentyPercent: UIButton!
+    
+    @IBOutlet weak var splitBy: UILabel!
+
+    @IBAction func tipUpdated(_ sender: UIButton) {
+    }
+    
+    @IBAction func splitBy(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+    }
+}
+
