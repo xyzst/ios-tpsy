609704ab20a4322dbd4e209a626212e6830e89d7
Darren Rambaud <xyzst@users.noreply.github.com>
2020-02-16 20:01:51 -0600

    Add method documentation
    Notate the min and max of UIStepper element, as of today (2-25)
    
    Related to: none

--- Tpsy/Controller/TipViewController.swift
+++ Tpsy/Controller/TipViewController.swift
@@ -46,6 +46,12 @@ class TipViewController: UIViewController {
         print(currentTip)
     }
     
+    
+    /// Couples the UIStepper element with the *splitBy* UILabel. As the stepper element is increased or
+    /// decreased, the current value held by the stepper is cascaded to the *splitBy* label.
+    ///
+    /// Current min value is 2 while the max value is 25
+    /// - Parameter sender: The UIStepper element defined for this view controller
     @IBAction func splitBy(_ sender: UIStepper) {
         splitBy.text = String(format: "%1.f", sender.value)
     }
